1724cb3accbc500db9c00e38fa469d68445159a0
Ian Chan <icha024@gmail.com>
2016-01-31 01:55:52 +0000

    Enabled config via env variable

--- src/main/java/com/clianz/authotp/AuthOtpApplication.java
+++ src/main/java/com/clianz/authotp/AuthOtpApplication.java
@@ -14,49 +14,51 @@ import static com.clianz.authotp.google.PasscodeGenerator.getSigningOracle;
 public class AuthOtpApplication {
 
 	private final static Logger LOGGER = Logger.getLogger(AuthOtpApplication.class.getName());
-	private static final String PORT_CONFIG = System.getenv("PORT");
-	private static final int DEFAULT_PORT = 8080;
 
 	private static final String JSON_CONTENT_TYPE = "application/json";
 	public static final String RESULT_SUCCESS = "{\"result\":\"success\"}";
 	public static final String RESULT_FAILED = "{\"result\":\"failed\"}";
-	public static final Integer FUTURE_INTERVALS = Integer.getInteger("otp.interval.future", 0);
-	public static final Integer PAST_INTERVALS = Integer.getInteger("otp.interval.past", 0);
-	public static final Integer PASSCODE_LENGTH = Integer.getInteger("otp.passcode.length", PasscodeGenerator.PASS_CODE_LENGTH);
 
 	public static void main(final String[] args) {
 		new AuthOtpApplication().start();
 	}
 
 	public void start() {
-		int port = DEFAULT_PORT;
-		if (PORT_CONFIG != null) {
-			port = Integer.parseInt(PORT_CONFIG);
-		}
+		Integer port = getEnvProperty("PORT", 8080);
+		Integer pastInterval = getEnvProperty("otp.interval.past", 0);
+		Integer futureInterval = getEnvProperty("otp.interval.future", 0);
+		Integer passcodeLength = getEnvProperty("otp.passcode.length", PasscodeGenerator.PASS_CODE_LENGTH);
+
 		LOGGER.info("Listening to port: " + port);
-		LOGGER.info("Max past interval: " + PAST_INTERVALS);
-		LOGGER.info("Max future interval: " + FUTURE_INTERVALS);
+		LOGGER.info("Max past interval: " + pastInterval);
+		LOGGER.info("Max future interval: " + futureInterval);
+		LOGGER.info("Passcode length: " + passcodeLength);
 
 		Undertow server = Undertow.builder().addHttpListener(port, "0.0.0.0")
 				.setHandler(Handlers.pathTemplate().add("/{key}/{code}", (AsyncHttpHandler) exchange -> {
 					String key = exchange.getQueryParameters().get("key").getFirst();
 					String code = exchange.getQueryParameters().get("code").getFirst();
 					PasscodeGenerator.Signer signer = getSigningOracle(key);
-					PasscodeGenerator passcodeGenerator = new PasscodeGenerator(signer, PASSCODE_LENGTH);
+					PasscodeGenerator passcodeGenerator = new PasscodeGenerator(signer, passcodeLength);
 
 					long time = System.currentTimeMillis() / 1000 / PasscodeGenerator.INTERVAL;
-					boolean success = passcodeGenerator.verifyTimeoutCode(code, time, FUTURE_INTERVALS, PAST_INTERVALS);
+					boolean success = passcodeGenerator.verifyTimeoutCode(code, time, futureInterval, pastInterval);
 
 					exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, JSON_CONTENT_TYPE);
 					if (success) {
 						exchange.getResponseSender().send(RESULT_SUCCESS);
-						LOGGER.info("Verification success.");
+						//LOGGER.info("Verification success.");
 					} else {
 						exchange.getResponseSender().send(RESULT_FAILED);
-						LOGGER.info("Verification failed.");
+						//LOGGER.info("Verification failed.");
 					}
 				})).build();
 		server.start();
+	}
+
+	private Integer getEnvProperty(String propName, Integer defaultVal) {
+		String propVal = System.getenv(propName);
+		return (propVal == null) ? Integer.getInteger(propName, defaultVal) : Integer.parseInt(propVal);
 	}
 
 	@FunctionalInterface
